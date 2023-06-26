1ba3f56f1c3eab60aeeebe9739a13c52ef0d2db2
Shadannamazi <89245846+Shadannamazi@users.noreply.github.com>
2023-06-25 22:43:21 -0700

    Initial Commit

---
+++ ParkMate/ParkMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkMateApp.swift
+//  ParkMate
+//
+//  Created by Shadan Namazi on 2023-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ParkMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
